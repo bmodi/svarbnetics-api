10d7524d91e31442cafafba28ebce2f8104dea67
Mithu Modi <bsmodi32@gmail.com>
2024-05-19 14:31:10 -0400

    Added sample H2 database on startup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO example (name) VALUES ('Sample Data');
+INSERT INTO example (name) VALUES ('Bat');
+INSERT INTO example (name) VALUES ('Human');
+INSERT INTO example (name) VALUES ('Fruit Fly');
