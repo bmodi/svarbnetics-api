762fa4605d4fecee1f623f89cce8d380bc2d649c
Mithu Modi <bsmodi32@gmail.com>
2024-06-06 23:37:35 -0400

    Move mybatis mapping into xml file

--- src/main/java/org/svarb/svarbneticsapi/mapper/OrganismMapper.java
+++ src/main/java/org/svarb/svarbneticsapi/mapper/OrganismMapper.java
@@ -1,29 +1,12 @@
 package org.svarb.svarbneticsapi.mapper;
 
-import org.apache.ibatis.annotations.*;
+import org.apache.ibatis.annotations.Mapper;
 import org.svarb.svarbneticsapi.model.Organism;
 
 import java.util.List;
 
 @Mapper
 public interface OrganismMapper {
-
-    @Select("SELECT * FROM organism")
-    @Results({
-        @Result(property = "id", column = "organism_id"),
-        @Result(property = "name", column = "name"),
-        @Result(property = "chromosomes", javaType = List.class, column = "organism_id",
-                many = @Many(select = "org.svarb.svarbneticsapi.mapper.ChromosomeMapper.findByOrganismId"))
-    })
     List<Organism> findAll();
-
-    @Select("SELECT * FROM organism WHERE organism_id = #{id}")
-    @Results({
-        @Result(property = "id", column = "organism_id"),
-        @Result(property = "name", column = "name"),
-        @Result(property = "chromosomes", javaType = List.class, column = "organism_id",
-                many = @Many(select = "org.svarb.svarbneticsapi.mapper.ChromosomeMapper.findByOrganismId"))
-    })
-    Organism findById(@Param("id") int id);
-
+    Organism findById(int id);
 }
--- src/test/java/org/svarb/svarbneticsapi/model/OrganismTest.java
+++ src/test/java/org/svarb/svarbneticsapi/model/OrganismTest.java
@@ -6,7 +6,6 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
